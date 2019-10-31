8d4a664ed2c6649be97f65b9e4bf7395e0232888
FitzpatrickAW2085 <56047029+FitzpatrickAW2085@users.noreply.github.com>
2019-10-31 13:00:34 -0700

    Add files via upload

---
+++ CashReg.py
@@ -0,0 +1,17 @@
+#No loop
+
+dollars = 0
+halfDollars = 0
+quarters = 0
+dimes = 0 
+pennies = 0
+
+
+print("Enter the price in dollars and cents")
+price = float(input("Enter the price:"))
+
+pennies = change * 100
+dollars = change * 1
+halfDollars = change * 2
+quarters = change * 4
+dimes = change * 10
